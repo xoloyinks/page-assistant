83103c7f4796a8eff54f0c75afae21442194e5ed
Kolawole Moses <xoloyinks@gmail.com>
2023-10-05 20:45:52 -0700

    Updated

--- src/page.js
+++ src/page.js
@@ -41,10 +41,6 @@ const socials = [
     }
 ]
 
-
-
-console.log(speechSynthesis.getVoices())
-
 export default function Home() {
     const [active, setActive] = useState(false);
     const handleActive = () => {
@@ -57,19 +53,16 @@ export default function Home() {
         speechSynthesis.speak(utterance);
     }
 
-    
-    
-
   return (
     <>
         <section className='w-screen h-screen parent backdrop-blur-2xl'>
-            <button onClick={handleActive} className='absolute z-50 px-5 py-3 rounded-full bg-gradient-to-br from-blue-900 to-yellow-700 bottom-10 right-7'>
+            <button onClick={handleActive} className='absolute z-50 hidden px-5 py-3 rounded-full lg:block bg-gradient-to-br from-blue-900 to-yellow-700 bottom-10 right-7'>
                 <span className='text-sm text-white'>
                     {active ? `Deactivate Assistant` : `Activate Assistant`}
                 </span>
             </button>
             <div className='absolute bottom-0 z-50 right-32'>
-                {active ? "" : <img  src={assistant} alt="" className=' h-[100px]' />}
+                {active ? "" : <img  src={assistant} alt="" className=' h-[100px] hidden lg:block' />}
             </div>
             <div className='w-screen h-screen backdrop-blur-2xl  py-8 px-[100px] flex items-center flex-col justify-center'>
                 <h1 className='text-2xl text-center text-zinc-300'>X-Assistant</h1>
@@ -80,12 +73,12 @@ export default function Home() {
                             return (
                                 <>
                                    <div className='relative'>
-                                        <a key={i} target='blank' href={datum.link} className='z-20 flex items-center gap-2 px-10 py-3 my-2 text-white rounded-lg bg-gradient-to-br from-blue-900 to-zinc-700'>{datum.social} <span className='text-xl'>{datum.icon}</span> </a>
+                                        <a key={i} target='blank' href={datum.link} className='z-20 flex items-center gap-2 px-10 py-3 my-2 text-sm text-white rounded-lg bg-gradient-to-br from-blue-900 to-zinc-700'>{datum.social} <span className='text-xl'>{datum.icon}</span> </a>
                                         {
-                                            active ? <div id='sib' className='absolute z-50 px-5 py-2 text-white rounded-r-xl rounded-tl-xl w-fit backdrop-blur-lg bg-gradient-to-tr bg-blue-950/50 -left-32 '>
+                                            active ? <div id='sib' className='absolute z-50 px-5 py-2 text-white shadow-inner shadow-blue-700 rounded-r-2xl rounded-tl-2xl w-fit backdrop-blur-lg bg-gradient-to-tr bg-blue-950/50 -left-32'>
                                             <div className='relative'>
-                                                 <span className='max-w-[50px]'>{datum.info}</span>
-                                                 <img  src={assistant} alt="" className='h-[150px] absolute top-0 -left-32'/>
+                                                 <span className='max-w-[50px] text-sm'>{datum.info}</span>
+                                                 <img  src={assistant} alt="" className='h-[150px] absolute top-0 -left-32 drop-shadow-xl'/>
                                             </div>
                                          </div>
                                          : ""
